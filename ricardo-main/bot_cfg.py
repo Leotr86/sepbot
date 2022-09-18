cc8fb0ea8c860d2f9386a2221832c7d42fbbddd0
Leotr86 <101873908+Leotr86@users.noreply.github.com>
2022-09-18 07:25:36 -0400

    Update bot_cfg.py

--- ricardo-main/bot_cfg.py
+++ ricardo-main/bot_cfg.py
@@ -15,7 +15,7 @@ def read_config() -> dict:
 tg_api_id = 12244448
 tg_api_hash = "890bdfa19a5ce152a64a80917fe7be29"
 tg_bot_token = "5478857996:AAGNVPRBE0VaOWqQD4CljEMf0tBUImDAjxA"
-datastore_store_id = -1001695628050
+datastore_store_id = -1001503019401
 datastore_field_id = 4
 global_config = {}
 
